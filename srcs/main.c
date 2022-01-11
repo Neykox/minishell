17e9f808b701eeec8dcd7d89dcf425b29dd23724
Nader EL MASRI <nel-masr@e2r2p16.clusters.42paris.fr>
2022-01-11 17:47:34 +0100

    jsp

--- srcs/main.c
+++ srcs/main.c
@@ -6,24 +6,30 @@
 /*   By: nel-masr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 15:53:56 by nel-masr          #+#    #+#             */
-/*   Updated: 2022/01/10 15:20:31 by nel-masr         ###   ########.fr       */
+/*   Updated: 2022/01/11 16:17:38 by nel-masr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/minishell.h"
+#include "../includes/minishell.h"
 
 int	main(int ac, char **av, char **envp)
 {
 	char	*line;
 	t_lxr	*lxr;
 
+	(void)ac;
+	(void)av;
+	(void)envp;
 	line = readline("minishell$ ");
 	add_history(line);
-	while (ret != NULL)
+	while (line != NULL)
 	{
+		lxr = NULL;
 		lxr = lexer(lxr, line);
+		print_lxr(lxr);
 		line = readline("minishell$ ");
 		add_history(line);
+		free(line);
 	}
 	return (0);
 }
