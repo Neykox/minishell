88036e12c9d58100c748887d180736ab7a4891c4
Nader EL MASRI <nel-masr@e3r9p15.clusters.42paris.fr>
2022-01-14 16:50:10 +0100

    Fix: lexer/parser should do its job well now

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: nel-masr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 18:14:20 by nel-masr          #+#    #+#             */
-/*   Updated: 2022/01/14 16:12:38 by nel-masr         ###   ########.fr       */
+/*   Updated: 2022/01/14 16:47:05 by nel-masr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@ void	print_parsing_error(t_lxr *lxr, int ret)
 {
 	if (ret == 1)
 	{
-		write(2, "minishell: syntax error near unexpected token ", 46);
-		write(2, &(lxr->next->value), ft_strlen(lxr->next->value));
+		write(2, "minishell: syntax error near unexpected token '", 47);
+		write(2, lxr->next->value, ft_strlen(lxr->next->value));
+		write(2, "'", 1);
 		write(2, "\n", 1);
-		printf("'%s'\n", lxr->next->value);
 	}
 	if (ret == 2)
 		write(2, "minishell: syntax error: uneven number of quotes\n", 49);
 	if (ret == 3)
 		write(2, "minishell: failed allocating proper memory for token values\n", 60);
+	if (ret == 4)
+	{
+		write(2, "minishell: syntax error near unexpected token '", 47);
+		write(2, lxr->next->next->value, ft_strlen(lxr->next->value));
+		write(2, "'", 1);
+		write(2, "\n", 1);
+	}	
 }
 
 int	syntax_checker(t_lxr *lxr)
@@ -40,7 +47,10 @@ int	syntax_checker(t_lxr *lxr)
 	{
 		if (!parser)
 			return (0);
-		if (parser->next)
+		/*
+		 * <| for example
+		 */
+		if (parser->next && parser->next->token != WSPACE)
 		{
 			if (parser->token == PIPE && parser->next->token != WORD)
 				return (1);
@@ -54,10 +64,19 @@ int	syntax_checker(t_lxr *lxr)
 				return (1);
 			if (parser->token == QUOTE_ERROR)
 				return (2);
-			/*if (parser->token == DQUOTE)
-				if (verify_quote(line, -1, 0, '"') == -1)
-					return (2);*/
-}
+		}
+		/*
+		 * < | for example, basically we look past the WSPACE token in parser->next->next
+		 */
+		else if (parser->next && parser->next->next)
+		{
+			if (parser->next->next->token != WORD && (parser->token == PIPE ||
+				parser->token == REDIR_STDIN || parser->token == REDIR_STDOUT ||
+				parser->token == DREDIR_LEFT || parser->token == DREDIR_LEFT))
+				return (4);
+			if (parser->token == QUOTE_ERROR)
+				return (2);
+		}
 		parser = parser->next;
 	}
 }
